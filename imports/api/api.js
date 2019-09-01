f2e0f2d9e55c87572d8356b0cd7fbcd5e558798e
Matthew Podolak <mpodola2@uwo.ca>
2019-09-01 18:35:27 -0400

    created menu api

---
+++ imports/api/api.js
@@ -0,0 +1,67 @@
+import { Restivus } from 'meteor/nimble:restivus';
+import { HTTP } from 'meteor/http';
+import { Meteor } from 'meteor/meteor';
+
+const customMenuData = require('../ui/menu/custom_json.json');
+const menuData = require('../ui/menu/menu.json');
+
+if (Meteor.isServer) {
+    // Global API configuration
+    var Api = new Restivus({
+      useDefaultAuth: true,
+      prettyJson: true
+    });
+
+    Api.addRoute('menu', {authRequired: false}, {
+        get: function () {
+            //return menu data to website for menu management
+            return {"status": "success", "menu": menuData}
+        },
+        post: function () {
+            var fs = require('fs');
+            var menu = {
+                pizza_deals: [],
+                specialty: [],
+                freedelivery: [],
+                wingsandsandwiches: [],
+                salads: [],
+                sides: [],
+                pitas: []
+            }
+            try{
+                //accept menu with updated statuses
+                var updatedMenu = this.bodyParams.menu;
+
+                var menuJSON = JSON.stringify(updatedMenu);
+                fs.writeFileSync('../../../../../imports/ui/menu/menu.json', menuJSON);
+
+                function checkItems(category){
+                    var items = updatedMenu[category]
+                    for(var i=0; i < items.length; i++){
+                        if(items[i].active){
+                            menu[category].push(items[i])
+                        }
+                    }
+                }
+
+                checkItems('freedelivery');
+                checkItems('pizza_deals');
+                checkItems('specialty');
+                checkItems('wingsandsandwiches');
+                checkItems('salads');
+                checkItems('sides');
+                checkItems('pitas');
+
+                var json = JSON.stringify(menu);
+                fs.writeFileSync('../../../../../imports/ui/menu/custom_json.json', json);
+
+            }
+            catch(e){
+                return {"status": "fail", "message": e}
+            }
+            
+            return {"status": "success"}
+        }
+    });
+
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import '../imports/api/carts.js';
 import '../imports/api/orders.js';
+import '../imports/api/api.js';
 
 AccountsGuest.anonymous = true
 
